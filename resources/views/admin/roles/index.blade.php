061f3c6fd626b1d2b71de0e8ca4a69e03decc54a
Fabio Vige <fabiovige@gmail.com>
2022-11-26 10:37:24 -0300

    #6 Display Roles and Permissions

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -1,11 +1,40 @@
 <x-admin-layout>
-    <div class="py-4">
+    <div class="py-4 w-full">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Role Content") }}
-                </div>
+            <div class="flex justify-end">
+                <a href="#" >Create</a>
             </div>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
+                <div class="p-6 text-gray-900">
+
+                    <table class="w-full">
+                        <thead>
+                            <tr>
+                                <th class="left">Nome</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($roles as $role)
+                            <tr>
+                                <td>{{ $role->name }}</td>
+                                <td>
+                                    <a href="#">Edit</a>
+                                    <a href="#">Delete</a>
+                                </td>
+                            </tr>
+                            @endforeach
+
+                        </tbody>
+
+                    </table>
+
+                </div>
+
+
+            </div>
+
         </div>
     </div>
 </x-admin-layout>
